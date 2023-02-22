c97851dc6fc5dff7290436fdff1e4b647904c954
Greg <gd.temp@pm.me>
2023-02-22 16:41:08 +0100

    Tests passed, but not with the best code

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(...arg) {
+    let finalArray = [];
 
+    for (let i = 0; i < arg.length; i++) {
+        if (typeof(arg[i]) !== "number") {
+            return 'ERROR'
+        }
+    }
+
+    if (arg[0] > arg[1]) {
+        arg.sort((a, b) => a - b);
+    }
+
+    for (let i = arg[0]; i <= arg[1]; i++) {
+        if (arg[i] < 0) {
+            return 'ERROR'
+        } else {
+            finalArray.push(i)
+        }
+    }
+
+    return finalArray.reduce((acc, cur) => acc + cur);
 };
 
 // Do not edit below this line
