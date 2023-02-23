b004ddc47d00d3bc0f48f3057e2fa0693f61d0b6
Greg <gd.temp@pm.me>
2023-02-23 19:22:40 +0100

    All tests have passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let finalTemp = temp;
+  finalTemp = (temp - 32) / 1.8;
+  finalTemp= Math.round(finalTemp * 10) / 10;
+  return finalTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let finalTemp = temp;
+  finalTemp = (temp * 1.8) + 32;
+  finalTemp = Math.round(finalTemp * 10) / 10;
+  return finalTemp;
 };
 
 // Do not edit below this line
