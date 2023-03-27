81a5b694345349979fe9102db72da5d08278c03e
Greg <gd.temp@pm.me>
2023-03-27 21:17:34 +0200

    Everything is done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,52 @@
-const add = function() {
-	
+const add = function(first, second) {
+    return first + second;
 };
 
-const subtract = function() {
-	
+const subtract = function(first, second) {
+    return first - second;
 };
 
-const sum = function() {
-	
+const sum = function(para) {
+    let interSum = 0;
+    for (let i = 0; i < para.length; ++i) {
+        interSum += para[i];
+    }
+    return interSum;
 };
 
-const multiply = function() {
-
+const multiply = function(para) {
+    let interSum = 1;
+    for (let i = 0; i < para.length; ++i) {
+        interSum *= para[i];
+    }
+    return interSum;
 };
 
-const power = function() {
-	
+const power = function(first, second) {
+    let interSum = 1;
+    for (let i = 1; i <= second; ++i) {
+        interSum *= first;
+    }
+    return interSum;
 };
 
-const factorial = function() {
-	
+const factorial = function(fact) {
+    let interSum = 1;
+    if (fact === 0) {
+        interSum = 1;
+    }
+    for (let i = 1; i <= fact; ++i) {
+        interSum *= i;
+    }
+    return interSum;
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial
 };
