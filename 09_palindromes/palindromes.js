7e2ff4951ab9bdffe3374008574c12fe193c36ea
Greg <gd.temp@pm.me>
2023-03-27 22:23:55 +0200

    Nailed it ^^ but with uggly code

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function () {
+const palindromes = function (input) {
 
+    let withOutPunct = input.replace(/[!,. ]/g,'');
+    console.log("punct = " + withOutPunct);
+
+    let toLower = withOutPunct.toLowerCase();
+    console.log("lower = " + toLower)
+
+    let splitString = toLower.split('');
+    console.log("split = " + splitString);
+
+    let reverseString = splitString.reverse();
+    console.log("reverse = " + reverseString);
+
+    let finalString = reverseString.join('');
+    console.log("final = " + finalString);
+
+    if (finalString === toLower) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
