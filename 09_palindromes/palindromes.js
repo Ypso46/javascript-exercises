ef1f29d64f3ee96f0999f4fdc1e220b7fccc2233
Greg <gd.temp@pm.me>
2023-03-28 09:33:32 +0200

    Minor refactor

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,25 +1,16 @@
 const palindromes = function (input) {
 
     let withOutPunct = input.replace(/[!,. ]/g,'');
-    console.log("punct = " + withOutPunct);
 
     let toLower = withOutPunct.toLowerCase();
-    console.log("lower = " + toLower)
 
     let splitString = toLower.split('');
-    console.log("split = " + splitString);
 
     let reverseString = splitString.reverse();
-    console.log("reverse = " + reverseString);
 
     let finalString = reverseString.join('');
-    console.log("final = " + finalString);
 
-    if (finalString === toLower) {
-        return true;
-    } else {
-        return false;
-    }
+    return finalString === toLower;
 };
 
 // Do not edit below this line
