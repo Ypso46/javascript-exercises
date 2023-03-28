d05164c9b781f61a6a0075e18d7a2401ffcf8bdd
Greg <gd.temp@pm.me>
2023-03-28 11:57:47 +0200

    Everything works fine

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
 const fibonacci = function(num) {
-    
+    let number = Number(num);
+    let oneBefore = 1;
+    let twoBefore = 0;
+    let sum = 0;
+    let count = 1;
+    if (number < 0) {
+        return "OOPS"
+    } else if (number === 1 || number === 2) {
+        return 1;
+    } else {
+        {
+            while (count !== number) {
+                sum = twoBefore + oneBefore;
+                twoBefore = oneBefore;
+                oneBefore = sum;
+                ++count;
+            }
+            return sum;
+        }
+    }
+
 };
 
 // Do not edit below this line
